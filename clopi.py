e40d2d7aaf2011f1941d7ea2f7b63c9ae56b2dd6
ierokirykas <zhadnovalexandr@gmail.com>
2025-11-05 19:16:33 +0300

    Сделал текстовый список грибов и чтение Python

--- clopi.py
+++ clopi.py
@@ -1,1 +1,7 @@
-#while true
+
+file = open("CLOPE_Loginom\mushrooms.txt","r")
+sp = []
+for item in file.readlines():
+    item = item.replace('\n','').split(',')
+    sp.append(item)
+print(sp[:20])
