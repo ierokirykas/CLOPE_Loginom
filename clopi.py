34f48dd773143fe7f4ad45b3553e8786223b5fa8
ierokirykas <zhadnovalexandr@gmail.com>
2025-11-06 23:40:10 +0300

    Добавил класс CLOPE и функцию расчёта прироста deltaAdd

--- clopi.py
+++ clopi.py
@@ -32,6 +32,7 @@ class Cluster:
     
     # Приводим в правильный синтаксис
     def sort_cluster(self): 
+        # Честно, я подсмотрел эту сортировку в инете. Сам бы до x:y не додумался :)
         self.transactions = {key:val for key,val in sorted(self.transactions.items(), key=lambda item: -item[1])}
 
     def visualize_cluster(self):
@@ -41,3 +42,26 @@ class Cluster:
         barplot(x=keys,y=vals)
         show()
 
+# Так. С кластерами разобрались. Теперь нужно сделать сам алгоритм.
+class CLOPE:
+    def __init__(self):
+        # Чё там ему надобно?
+        self.clusters = {}
+        self.count_iterations = 0
+        self.iterations = {}
+        self.counter = 0
+    # И самое основное - Profit(C,r). Который мы переделываем в DeltaAdd(C,t,r)
+    # Где C - сама транзакция, t - номер кластера, r - отталкивание кластеров. В примере r = 2.6
+    def deltaAdd(self,C,t,r):
+        # Для начала нужно посчитать новую площадь
+        area_new = self.clusters[t].area + len(C)
+        # И ещё ща будем считать новые широты (ахах)
+        width_new = self.clusters[t].width
+        for item in C:
+            if not(item in self.clusters[t].transactions):
+                width_new += 1
+            results = area_new * (self.clusters[t].counter + 1)/pow(width_new,r) 
+            - self.clusters[t].area * self.clusters[t].counter/pow(self.clusters[t].width,r)
+            # Чё-то типа такого, судя по методе.
+
+            
--- main.py
+++ main.py
@@ -15,11 +15,3 @@ print(C1.transactions)
 print(f'Площадь = {C1.area} \nШирина = {C1.width} \nГрадиент = {C1.gradient}')
 
 C1.visualize_cluster()
-
-C2 = Cluster()
-C2.add_transaction(['d','e'])
-C2.add_transaction(['d','e','f'])
-print(C2.transactions)
-print(f'Площадь = {C2.area} \nШирина = {C2.width} \nГрадиент = {C2.gradient}')
-
-C2.visualize_cluster()
