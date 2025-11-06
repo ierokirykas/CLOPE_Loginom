6be767934fbc166878d86519638c2fc266949f22
ierokirykas <zhadnovalexandr@gmail.com>
2025-11-06 21:29:03 +0300

    Визуализируем

--- clopi.py
+++ clopi.py
@@ -1,3 +1,6 @@
+import seaborn as sns # pip install seaborn
+import matplotlib.pyplot as plt
+
 # Пробуем прочитать файл
 file = open("CLOPE_Loginom\\mushrooms.txt","r") #хехе, грибы
 Database = []
@@ -32,8 +35,6 @@ S = sum([sum(_.values()) for _ in unique_stats])
 # H(C) - вычисляем по формуле S(C)/W(C)
 # Хмм, а как насчёт сделать визуализацию? Попробую seaborn
 '''
-import seaborn as sns # pip install seaborn
-import matplotlib.pyplot as plt
 keys = list(unique_stats[1].keys())
 vals = list(unique_stats[1].values())
 sns.barplot(x=keys,y=vals)
@@ -47,7 +48,8 @@ class Cluster:
         # Чё там у него должно быть?
         # Площадь, высота, ширина. Вроде всё
         self.area = 0
-        self.height = 0
+        # self.height = 0 не нужен по причине того, что градиент более эффективен
+        self.gradient = 0
         self.width = 0
         # Ну и сами элементы кластера, очевидно =|
         self.transactions = {}
@@ -61,7 +63,32 @@ class Cluster:
         for item in transaction:
             if not(item in self.transactions):
                 # print(item)
-                unique_stats[index_counter][item] = 1
+                self.transactions[item] = 1
             else:
-                unique_stats[index_counter][item] += 1
+                self.transactions[item] += 1
         self.counter += 1
+        # Не забываем обновлять параметры кластера
+        self.area += float(len(transaction)) # Они всё равно все одинакового размера. Но потом будем их укорачивать
+        self.width = float(len(self.transactions))
+        self.gradient = self.area/pow(self.width,2)
+
+# Ща посмотрим как это выглядит
+CC = Cluster()
+CC.add_transaction(['a','b'])
+CC.add_transaction(['a','b','c'])
+CC.add_transaction(['a','c','d'])
+CC.add_transaction(['d','e'])
+CC.add_transaction(['d','e','f'])
+print(CC.transactions)
+print(f'Площадь = {CC.area} \nШирина = {CC.width} \nГрадиент = {CC.gradient}')
+
+# Не хочу писать одно и тоже каждый раз, когда нужно визуализировать. Сделаем функцию
+def visualize_cluster(transactions):
+    transactions = sort_cluster(transactions)
+    keys = list(transactions.keys())
+    vals = list(transactions.values())
+    sns.barplot(x=keys,y=vals)
+    plt.show()
+# Ещё нужно сделать сортировку
+def sort_cluster(transactions): return {key:val for key,val in sorted(transactions.items(), key=lambda item: -item[1])}
+visualize_cluster(CC.transactions)
